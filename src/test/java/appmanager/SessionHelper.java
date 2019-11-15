a2ed7a5ed9f33904b20ffd8c6c5a2126b847d414
SergeyLashenko <sergeylashenco@gmail.com>
2019-11-15 22:01:48 +0200

    Доп Коммит, добавленна проверка и закрытие Браузера если не запущен XAMP

--- src/test/java/appmanager/SessionHelper.java
+++ src/test/java/appmanager/SessionHelper.java
@@ -6,15 +6,25 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class SessionHelper extends HelperBase{
 
+
+
    SessionHelper(WebDriver wb) {
       super( wb);
    }
 
 
    public void login(String userName, String password) {
-      wb.get("http://localhost/addressbook/index.php");
-      type(By.name("user") , userName);
-      type( By.name("pass") , password );
-      click(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]"));
+
+      try {
+         wb.get("http://localhost/addressbook/index.php");
+         type(By.name("user") , userName);
+         type( By.name("pass") , password );
+         click(By.xpath(
+                 "(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]"));
+      } catch (Exception ex) {
+         System.out.println(" На данный момент не запущен XAMP авторизация не возможна");
+        ex.printStackTrace();
+        wb.quit();
+      }
    }
 }
