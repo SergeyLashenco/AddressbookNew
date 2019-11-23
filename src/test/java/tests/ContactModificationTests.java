c69122a282faba40a08f26e6cf99786a20389c73
SergeyLashenko <sergeylashenco@gmail.com>
2019-11-23 15:02:13 +0200

    Реализована проверка и выполнение предусловиий в тесте по Модификации Контакта

--- src/test/java/tests/ContactModificationTests.java
+++ src/test/java/tests/ContactModificationTests.java
@@ -8,6 +8,9 @@ public class ContactModificationTests extends  TestBase {
    @Test
    public  void  testContactModification () {
       app.getNavigationHelper().gotoHomePage();
+      if (!app.getContactHelper().isThereAContact()){
+         app.getContactHelper().creationContact(new ContactData("TestContact1", "TestContact1", "test1"), true);
+      }
       app.getContactHelper().initContactModification();
       app.getContactHelper().fillContactForm(
               new ContactData("test_name", "test_surname",null), false);
