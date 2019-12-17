4d607cee26fc384428287dcae6aa346224ed5f51
SergeyLashenko <sergeylashenco@gmail.com>
2019-12-17 23:17:21 +0200

    GroupCreatTests и ПерваяЛямда

--- src/test/java/tests/GroupCreatTests.java
+++ src/test/java/tests/GroupCreatTests.java
@@ -4,16 +4,23 @@ import model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.HashSet;
 import java.util.List;
 
-public class GroupCreatTests extends  TestBase {
+public class GroupCreatTests extends TestBase {
 
    @Test
    public void testGroupCreation() throws Exception {
       app.getNavigationHelper().gotoGroupPage();
       List<GroupData> before = app.getGroupHelper().getGroupList();
-      app.getGroupHelper().createGruop(new GroupData("testlast", null, null));
+      GroupData group = new GroupData("test7", null, null);
+      app.getGroupHelper().createGruop(group);
       List<GroupData> after = app.getGroupHelper().getGroupList();
-      Assert.assertEquals(after.size() , before.size() +1);
+      Assert.assertEquals(after.size(), before.size() + 1);
+
+      group.setId(after.stream().max((groupData, t1)
+              -> Integer.compare(groupData.getId(), t1.getId())).get().getId());
+      before.add(group);
+      Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
    }
 }
