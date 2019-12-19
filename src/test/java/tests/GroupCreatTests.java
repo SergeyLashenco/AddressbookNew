517793acb0a1953dbe9bd93216dcdabf1d832d63
SergeyLashenko <sergeylashenco@gmail.com>
2019-12-19 23:36:33 +0200

    Рефактор name-Методов и рефакторТестов

--- src/test/java/tests/GroupCreatTests.java
+++ src/test/java/tests/GroupCreatTests.java
@@ -5,22 +5,21 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.Comparator;
-import java.util.HashSet;
 import java.util.List;
 
 public class GroupCreatTests extends TestBase {
 
    @Test
-   public void testGroupCreation() throws Exception {
-      app.getNavigationHelper().gotoGroupPage();
-      List<GroupData> before = app.getGroupHelper().getGroupList();
+   public void testGroupCreation() {
+      app.goTo().groupPage();
+      List<GroupData> before = app.group().list();
       GroupData group = new GroupData("test7", null, null);
-      app.getGroupHelper().createGruop(group);
-      List<GroupData> after = app.getGroupHelper().getGroupList();
+      app.group().create(group);
+      List<GroupData> after = app.group().list();
       Assert.assertEquals(after.size(), before.size() + 1);
 
       before.add(group);
-      Comparator<? super GroupData> byId = (g1 , g2 ) -> Integer.compare(g1.getId() , g2.getId());
+      Comparator<? super GroupData> byId = Comparator.comparingInt(GroupData::getId);
       before.sort(byId);
       after.sort(byId);
       Assert.assertEquals(before, after);
