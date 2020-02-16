8889920b0dd097a2afa462a1c50fb6b4d18234e3
SergeyLashenko <sergeylashenco@gmail.com>
2020-02-16 13:09:45 +0200

    Add DbHelper

--- src/test/java/tests/GroupModificationTest.java
+++ src/test/java/tests/GroupModificationTest.java
@@ -2,42 +2,37 @@ package tests;
 
 import model.GroupData;
 import model.Groups;
-import org.hamcrest.CoreMatchers;
-import org.hamcrest.MatcherAssert;
-import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.util.Set;
-
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.testng.Assert.assertEquals;
 
-public class GroupModificationTest extends  TestBase {
+public class GroupModificationTest extends TestBase {
 
-   @BeforeMethod
-   public void ensurePreconditions(){
-      app.goTo().groupPage();
-      if (app.group().all().size() == 0){
-         app.group().create(new GroupData().withName("test1"));
-      }
-   }
+    @BeforeMethod
+    public void ensurePreconditions() {
+        if (app.db().groups().size() == 0) {
+            app.goTo().groupPage();
+            app.group().create(new GroupData().withName("test1"));
+        }
+    }
 
-   @Test (priority = 2)
-   public  void testGroupModification(){
+    @Test(priority = 2)
+    public void testGroupModification() {
 
-      Groups before = app.group().all();
-      GroupData modificationGroup = before.iterator().next();
-      GroupData group = new GroupData()
-              .withId(modificationGroup.getId())
-              .withName("test1")
-              .withHeader("test2")
-              .withFooter("test3");
-      app.group().modify(group);
-      assertThat(app.group().count(), equalTo( before.size()));
-      Groups after = app.group().all();
-      assertThat(after, equalTo(before.without(modificationGroup).withAdded(group)));
-   }
+        Groups before = app.db().groups();
+        GroupData modificationGroup = before.iterator().next();
+        GroupData group = new GroupData()
+                .withId(modificationGroup.getId())
+                .withName("test1")
+                .withHeader("test2")
+                .withFooter("test3");
+        app.goTo().groupPage();
+        app.group().modify(group);
+        assertThat(app.group().count(), equalTo(before.size()));
+        Groups after = app.db().groups();
+        assertThat(after, equalTo(before.without(modificationGroup).withAdded(group)));
+    }
 
 }
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,23 +1,17 @@
 package tests;
 
 import appmanager.ApplicationManager;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-import model.GroupData;
 import org.openqa.selenium.remote.BrowserType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class TestBase {
    Logger logger = LoggerFactory.getLogger(TestBase.class);
